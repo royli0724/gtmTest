9bd9c0b674672a286dcb6baa8f7f91238a4f1130
RoyLi <royli@damaiapp.com.tw>
2025-01-17 18:10:45 +0800

    feat: add element plus

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ElMessage } from "element-plus";
+
+function sendEvent() {
+  ElMessage({
+    message: "send home_event",
+    type: "success",
+    plain: true,
+  });
+
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: "home_event",
+    data: "this is test data",
+  });
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>home</div>
+  <div class="bigBox">this is big box</div>
+  <el-button plain @click="sendEvent()">sendEvent</el-button>
 </template>
+
+<style>
+.bigBox {
+  height: 150vh;
+  display: flex;
+  align-items: center;
+  margin-bottom: 30px;
+  background: rgb(239, 238, 237);
+}
+</style>
